675bbd95ba5a8f6f5f411b4e6b714844c64617f7
Howard Lightfoot <howard.lightfoot@sanger.ac.uk>
2018-10-26 15:36:34 +0100

    Trying to fix presentation of concs in heatmap.

--- components/matrix_viability.py
+++ components/matrix_viability.py
@@ -3,7 +3,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
-
 from app import app
 
 
@@ -13,10 +12,14 @@ def layout(matrix):
     matrix_df = pd.DataFrame([w.to_dict() for w in matrix.well_results])
 
     matrix_df = matrix_df.assign(
-        lib1_dose=matrix_df.lib1_dose.str.extract(r'D(?P<lib1_dose>\d+)'),
-        lib2_dose=matrix_df.lib2_dose.str.extract(r'D(?P<lib2_dose>\d+)'),
+    #     # lib1_dose=matrix_df.lib1_dose.str.extract(r'D(?P<lib1_dose>\d+)'),
+    #     # lib2_dose=matrix_df.lib2_dose.str.extract(r'D(?P<lib2_dose>\d+)'),
+    #     lib1_dose=matrix_df.lib1_conc,
+    #     lib2_dose=matrix_df.lib2_conc,
+    #     lib1_conc=matrix_df.lib1_conc / 1000000,
+    #     lib2_conc=matrix_df.lib2_conc / 1000000,
         inhibition=lambda df: 1 - df.viability)
-    matrix_df = matrix_df[['lib1_dose', 'lib2_dose'] + available_viability_metrics]
+    matrix_df = matrix_df[['lib1_conc', 'lib2_conc'] + available_viability_metrics]
 
     return html.Div(className='row', children=[
         html.Div(className='col-6', children=[
@@ -42,20 +45,47 @@ def layout(matrix):
 )
 def update_viability_heatmap(viability_heatmap_zvalue, matrix_json):
     matrix_df = pd.read_json(matrix_json, orient='split')
+
+    matrix_df['lib1_conc'] = matrix_df['lib1_conc'].astype('category')
+    matrix_df['lib2_conc'] = matrix_df['lib2_conc'].astype('category')
+
     zvalue = matrix_df[viability_heatmap_zvalue]
 
     return {
         'data': [
             go.Heatmap(
-                x=matrix_df.lib1_dose,
-                y=matrix_df.lib2_dose,
+                x=matrix_df.lib1_conc,
+                y=matrix_df.lib2_conc,
                 z=zvalue,
                 zmax=1,
                 zmin=0,
                 colorscale='Viridis'
             )
         ],
-        'layout': go.Layout(title=viability_heatmap_zvalue)
+        'layout': go.Layout(title=viability_heatmap_zvalue,
+                            xaxis={'type': 'category',
+                                   'title': 'Concentration (uM)'
+                                   },
+                            yaxis={'type': 'category',
+                                   'title': 'Concentration (uM)'
+                                   }
+
+                            )
+        # 'layout': go.Layout(title=viability_heatmap_zvalue,
+        #                     xaxis={'type': '-',
+        #                            'title': 'Concentration (uM)',
+        #                            'tickmode': 'array',
+        #                            'tickvals': matrix_df.lib1_conc.unique(),
+        #                            'ticktext': matrix_df.lib1_conc.unique()
+        #                            },
+        #                     yaxis={'type':'-',
+        #                            'title': 'Concentration (uM)',
+        #                            'tickmode': 'array',
+        #                            'tickvals': matrix_df.lib1_conc.unique(),
+        #                            'ticktext': matrix_df.lib1_conc.unique()
+        #                            }
+        #
+        #                     )
     }
 
 
@@ -66,8 +96,8 @@ def update_viability_heatmap(viability_heatmap_zvalue, matrix_json):
 )
 def update_viability_surface(viability_heatmap_zvalue, matrix_json):
     matrix_df = pd.read_json(matrix_json, orient='split')
-    zvalue = matrix_df[['lib1_dose', 'lib2_dose', viability_heatmap_zvalue]]\
-        .pivot(index='lib2_dose', columns='lib1_dose',
+    zvalue = matrix_df[['lib1_conc', 'lib2_conc', viability_heatmap_zvalue]]\
+        .pivot(index='lib2_conc', columns='lib1_conc',
                values=viability_heatmap_zvalue)
 
     return {
