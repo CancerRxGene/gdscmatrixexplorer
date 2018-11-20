3a9501f3a079fcdfef32168be02533e7fc3c83f6
Donny van der Meer <dvm@sanger.ac.uk>
2018-11-20 22:01:21 +0000

    Add Breadcrumbs to matrix page and bugfix for normal models

--- pages/matrix.py
+++ pages/matrix.py
@@ -5,17 +5,22 @@ from db import session
 
 from models import MatrixResult
 from components.matrix_intro import layout as intro
-from components.matrix_table import layout as table
 from components.matrix_viability import layout as viability
 from components.matrix_synergy import layout as synergy
+from components.breadcrumbs import breadcrumb_generator as crumbs
 
 
 def layout(barcode=None, cmatrix=None):
     my_matrix = session.query(MatrixResult) \
         .filter_by(barcode=barcode, cmatrix=cmatrix).one()
+    p = my_matrix.project
+    c = my_matrix.combination
 
     return html.Div([
         dcc.Location(id='matrix-url', refresh=True),
+        crumbs([("Home", "/"), (p.name, f"/project/{p.slug}"),
+                (f"{c.lib1.drug_name} + {c.lib2.drug_name}", f"/project/{p.slug}/combination/{c.lib1_id}+{c.lib2_id}"),
+                (f"{my_matrix.model.name}",)]),
         intro(my_matrix),
         viability(my_matrix),
         synergy(my_matrix),
