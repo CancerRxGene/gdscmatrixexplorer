89d741efe1772446fe45eb9e33dd4da7342aa008
howard-lightfoot <hl7@sanger.ac.uk>
2018-11-27 17:27:23 +0000

    Updated requirements for pip and index to run with gunicorn

--- index.py
+++ index.py
@@ -29,6 +29,7 @@ app.layout = html.Div([
 
     ])
 
+server = app.server
 
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
@@ -53,4 +54,4 @@ def display_page(pathname):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=8080, host='0.0.0.0')
