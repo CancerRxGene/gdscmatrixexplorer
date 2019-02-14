49070f2b3d4a62f1af9d20e46000b075ac0597ce
Donny van der Meer <dvm@sanger.ac.uk>
2019-02-14 15:02:41 +0000

    Add loading spinner

--- page_components.py
+++ page_components.py
@@ -10,11 +10,11 @@ header = dbc.Navbar(
         dbc.Container([
             dbc.Row(className='w-100 align-items-center', children=[
                 dbc.Col(width=2, children=[
-                    dbc.NavbarBrand(children=html.Img(src="assets/sanger_logo.png", height='50rem'), href="https://sanger.ac.uk"),
+                    dbc.NavbarBrand(children=html.Img(src="/assets/sanger_logo.png", height='50rem'), href="https://sanger.ac.uk"),
 
                 ]),
                 dbc.Col(width=2, children=[
-                    dbc.NavbarBrand(children=html.Img(src="assets/logo_white.svg", height='40rem'), href="/"),
+                    dbc.NavbarBrand(children=html.Img(src="/assets/logo_white.svg", height='40rem'), href="/"),
                 ]),
                 dbc.Col(width=8, className="d-flex align-items-center justify-content-end", children=[
                     dbc.Nav([
