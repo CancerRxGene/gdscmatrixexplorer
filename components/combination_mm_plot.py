1956440124709ad071e7ead382daf7d5d139d65b
Donny van der Meer <dvm@sanger.ac.uk>
2019-03-07 11:27:08 +0000

    Improve mm-plot with cache and multi-output callback

--- components/combination_mm_plot.py
+++ components/combination_mm_plot.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
@@ -11,12 +13,14 @@ from utils import url_is_combination_page, get_project_metrics, \
     get_combination_from_url, matrix_hover_label
 
 
+@lru_cache(maxsize=128)
 def get_plot_data_from_url(url):
     combination = get_combination_from_url(url)
     plot_data = get_combination_results_with_sa(combination)
     return plot_data
 
 
+@lru_cache(maxsize=128)
 def get_project_metrics_from_url(url, metric):
     project = get_project_from_url(url)
     return get_project_metrics(project.id, metric)
@@ -51,45 +55,47 @@ def layout():
 
 
 @app.callback(
-    dash.dependencies.Output('intxn-distn', 'figure'),
-    [dash.dependencies.Input('url', 'pathname'),
-     dash.dependencies.Input('combo-page-color-scale-select', 'value')])
-def update_distn(pathname, colorscale_select):
+    [dash.dependencies.Output('intxn-distn', 'figure'),
+     dash.dependencies.Output('combo-page-mm-scatter', 'figure'),
+     dash.dependencies.Output('combo-tissue', 'figure')],
+    [dash.dependencies.Input('combo-page-color-scale-select', 'value')],
+    [dash.dependencies.State('url', 'pathname')])
+def update_plots(colorscale_select, pathname):
     if not url_is_combination_page(pathname):
         return None
     plot_data = get_plot_data_from_url(pathname)
     project_metrics = get_project_metrics_from_url(pathname, colorscale_select)
 
-    drug1 = plot_data['lib1_name'].unique()
-    drug2 = plot_data['lib2_name'].unique()
+    drug1_name = plot_data['lib1_name'].unique()[0]
+    drug2_name = plot_data['lib2_name'].unique()[0]
 
-    figure = ff.create_distplot([plot_data[colorscale_select].dropna(), project_metrics[colorscale_select].dropna()],
-                       group_labels=[f"{drug1[0]}+{drug2[0]}", "All combinations"],
-                       show_rug=False,
-                       bin_size=round(
-                           (project_metrics[colorscale_select].dropna().max() -
-                            project_metrics[colorscale_select].dropna().min()) / 20,
-                           2)
-                       )
+    distplot = generate_distplot(project_metrics, plot_data, colorscale_select, drug1_name, drug2_name)
+    mm_scatter = generate_mm_scatter(project_metrics, plot_data, colorscale_select, drug1_name, drug2_name)
+    combo_tissues = generate_combo_tissue_plot(plot_data, colorscale_select)
+
+    return distplot, mm_scatter, combo_tissues
+
+
+def generate_distplot(project_metrics, plot_data, plot_metric, drug1_name, drug2_name):
+
+    figure = ff.create_distplot(
+        [plot_data[plot_metric].dropna(), project_metrics[plot_metric].dropna()],
+        group_labels=[f"{drug1_name} + {drug2_name}", "All combinations"],
+        show_rug=False,
+        bin_size=round(
+           (project_metrics[plot_metric].dropna().max() -
+            project_metrics[plot_metric].dropna().min()) / 20,
+           2)
+)
 
     return figure
 
-@app.callback(
-    dash.dependencies.Output('combo-page-mm-scatter', 'figure'),
-    [dash.dependencies.Input('url', 'pathname'),
-     dash.dependencies.Input('combo-page-color-scale-select', 'value')])
-def update_scatter(pathname, colorscale_select):
-    if not url_is_combination_page(pathname):
-        return None
-    plot_data = get_plot_data_from_url(pathname)
-    project_metrics = get_project_metrics_from_url(pathname, colorscale_select)
 
-    drug1 = plot_data['lib1_name'].unique()
-    drug2 = plot_data['lib2_name'].unique()
+def generate_mm_scatter(project_metrics, plot_data, plot_metric, drug1_name, drug2_name):
 
     # Set colorscale using whole project dataset
-    color_min = project_metrics[colorscale_select].min()
-    color_max = project_metrics[colorscale_select].max()
+    color_min = project_metrics[plot_metric].min()
+    color_max = project_metrics[plot_metric].max()
     color_zero = abs(0 - color_min / (color_max - color_min))
 
     return go.Figure({
@@ -100,7 +106,7 @@ def update_scatter(pathname, colorscale_select):
             opacity=0.7,
             marker={
                 'size': 8,
-                'color': plot_data[colorscale_select],
+                'color': plot_data[plot_metric],
                 'colorscale': [
                     [0, 'rgb(0, 0, 255)'],
                     [color_zero, 'rgb(255, 255, 255)'],
@@ -116,44 +122,43 @@ def update_scatter(pathname, colorscale_select):
         )
         ],
         'layout': {
-            'title': matrix_metrics[colorscale_select]['description'],
-            'xaxis': {'title': f"{drug1[0]} IC50 log µM"},
-            'yaxis': {'title': f"{drug2[0]} IC50 log µM"}
+            'title': matrix_metrics[plot_metric]['description'],
+            'xaxis': {'title': f"{drug1_name} IC50 log µM"},
+            'yaxis': {'title': f"{drug2_name} IC50 log µM"}
         }
     })
 
 
-@app.callback(
-    dash.dependencies.Output('combo-tissue', 'figure'),
-    [dash.dependencies.Input('url', 'pathname'),
-     dash.dependencies.Input('combo-page-color-scale-select', 'value')
-     ])
-def update_tissue_plot(url, colorscale_select):
-    if not url_is_combination_page(url):
-        return None
-    plot_data = get_plot_data_from_url(url)
+def generate_combo_tissue_plot(plot_data, plot_metric):
+
+    boxes = []
+
+    for tissue in (plot_data[['tissue', plot_metric]]
+                       .groupby(by='tissue', as_index=False)
+                       .median()
+                       .sort_values(by=plot_metric, ascending=False)
+                       .tissue):
+        box_data = plot_data[plot_data.tissue == tissue]
+        boxes.append(
+            go.Box(
+                name=tissue,
+                y=box_data[plot_metric],
+                boxpoints="all",
+                hoverinfo="y+text",
+                hoveron="points",
+                jitter=0.3,
+                marker={
+                    'size': 8
+                },
+                text=matrix_hover_label(box_data),
+                customdata=[(row.barcode, row.cmatrix) for row in
+                            box_data.itertuples(index=False)]
+            )
+        )
 
     return go.Figure(
-        data=[go.Box(
-            name=plot_data.query("tissue == @tissue").tissue.unique()[0],
-            y=plot_data.query("tissue == @tissue")[colorscale_select],
-            boxpoints="all",
-            hoverinfo="y+text",
-            hoveron="points",
-            jitter=0.3,
-            marker={
-                'size': 8
-            },
-            text=matrix_hover_label(plot_data),
-            customdata=[(row.barcode, row.cmatrix) for row in
-                        plot_data.itertuples(index=False)]
-        ) for tissue in plot_data[['tissue', colorscale_select]].
-            groupby(by='tissue', as_index=False)
-            .median()
-            .sort_values(by=colorscale_select, ascending=False)
-            .tissue
-        ],
-        layout= {
+        data=boxes,
+        layout={
             'title': 'Combination interaction effects by tissue type',
             'showlegend': False
         }
