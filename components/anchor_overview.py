936bb99057891eac093ad541f1456726a3035d90
wy1 <wy1@sanger.ac.uk>
2021-01-25 11:54:18 +0000

    add delta ic50 & delta emax plots

--- components/anchor_overview.py
+++ components/anchor_overview.py
@@ -13,6 +13,8 @@ from db import session
 from models import AnchorCombi
 
 def layout(combination):
+    anc_df = get_anc_df(combination)
+
     return html.Div(children=[
         dbc.Row(
             dbc.Col(width=12, children=[
@@ -22,56 +24,74 @@ def layout(combination):
             )]
         )),
          dbc.Row([
-            dbc.Col(width=5,children=[
+            dbc.Col(width=6,children=[
                 dcc.Loading(className='gdsc-spinner', children=
                     dcc.Graph(
                         id='box1',
-                        figure= generate_vialibity_boxplot(combination,'low')
+                        figure= generate_vialibity_boxplot(anc_df,'low')
                     )
                 )
             ]),
 
-            dbc.Col(width=4, children=[
-                dcc.Loading(className='gdsc-spinner', children=
-                dcc.Graph(
-                        id='box2',
-                        figure = generate_ic50_boxplot(combination,'low')
-                    )
-                )
-            ])
-             ]
-         ),
-        dbc.Row([
             dbc.Col(width=5, children=[
                 dcc.Loading(className='gdsc-spinner', children=
                 dcc.Graph(
-                    id='box3',
-                    figure=generate_vialibity_boxplot(combination,'high')
+                        id='box2',
+                        figure = generate_ic50_boxplot(anc_df,'low')
+                    )
+                )
+            ]),
+             dbc.Col(width=4, children=[
+                 dcc.Loading(className='gdsc-spinner', children=
+                 dcc.Graph(
+                     id='box3',
+                     figure=generate_delta_emax_boxplot(anc_df, 'low')
+                 )
+                             )
+             ]),
+             dbc.Col(width=4, children=[
+                 dcc.Loading(className='gdsc-spinner', children=
+                 dcc.Graph(
+                     id='box4',
+                     figure=generate_delta_ic50_boxplot(anc_df, 'low')
+                 )
+                             )
+             ])
+             ]
+         ),
+        dbc.Row([
+            dbc.Col(width=6, children=[
+                dcc.Loading(className='gdsc-spinner', children=
+                dcc.Graph(
+                    id='box5',
+                    figure=generate_vialibity_boxplot(anc_df,'high')
+                ))
+            ]),
+            dbc.Col(width=5, children=[
+                dcc.Loading(className='gdsc-spinner', children=
+                dcc.Graph(
+                    id='box6',
+                    figure=generate_ic50_boxplot(anc_df, 'high')
                 ))
             ]),
             dbc.Col(width=4, children=[
                 dcc.Loading(className='gdsc-spinner', children=
                 dcc.Graph(
-                    id='box4',
-                    figure=generate_ic50_boxplot(combination, 'high')
+                    id='box7',
+                    figure=generate_delta_emax_boxplot(anc_df, 'high')
                 ))
             ]),
-
+            dbc.Col(width=4, children=[
+                dcc.Loading(className='gdsc-spinner', children=
+                dcc.Graph(
+                    id='box8',
+                    figure=generate_delta_ic50_boxplot(anc_df, 'high')
+                ))
+            ]),
         ]),
     ])
 
-def generate_vialibity_boxplot(combination,anc_conc_type):
-    project_id = combination.project_id
-    lib_id = combination.lib1_id
-    anchor_id = combination.lib2_id
-
-    anchor_combi_query = session.query(AnchorCombi).\
-        filter(AnchorCombi.project_id == project_id).\
-        filter(AnchorCombi.library_id ==  lib_id).\
-        filter(AnchorCombi.anchor_id == anchor_id)
-
-    anc_df = pd.read_sql(anchor_combi_query.statement, session.bind)
-
+def generate_vialibity_boxplot(anc_df,anc_conc_type):
     anchor_conc = anc_df['anchor_conc'].drop_duplicates().sort_values()
 
     if(anc_conc_type == 'low'):
@@ -88,24 +108,14 @@ def generate_vialibity_boxplot(combination,anc_conc_type):
     title = 'Anchor '+ anc_conc_type
     final_df = anc_via_df.append(lib_emax_df).append(bliss_emax_df).append(combo_emax_df)
 
-    fig = px.strip(final_df, x='type', y='Viability %', title= title)
+    fig = px.strip(final_df, x='type', y='Viability %', title= title,
+                   labels= {'type':' '})
     # fig = px.box(final_df, x='type', y='Viability %', title=title,points="all",
     #                hover_data=["type", "Viability %", "cell_line_name"])
 
     return fig
 
-def generate_ic50_boxplot(combination,anc_conc_type):
-    project_id = combination.project_id
-    lib_id = combination.lib1_id
-    anchor_id = combination.lib2_id
-
-    anchor_combi_query = session.query(AnchorCombi). \
-        filter(AnchorCombi.project_id == project_id). \
-        filter(AnchorCombi.library_id == lib_id). \
-        filter(AnchorCombi.anchor_id == anchor_id)
-
-    anc_df = pd.read_sql(anchor_combi_query.statement, session.bind)
-
+def generate_ic50_boxplot(anc_df,anc_conc_type):
     anchor_conc = anc_df['anchor_conc'].drop_duplicates().sort_values()
 
     if (anc_conc_type == 'low'):
@@ -120,9 +130,37 @@ def generate_ic50_boxplot(combination,anc_conc_type):
     title = 'Anchor ' + anc_conc_type
     final_df = lib_ic50_df.append(combo_ic50_df)
 
-    fig = px.strip(final_df, x='type', y='Norm. drug conc', title = ' ')
+    fig = px.strip(final_df, x='type', y='Norm. drug conc.', title = ' ', labels= {'type':' '})
     # fig = px.box(final_df, x='type', y='Norm. drug conc', title=title, points="all")
 
+    return fig
+
+def generate_delta_emax_boxplot(anc_df,anc_conc_type):
+    anchor_conc = anc_df['anchor_conc'].drop_duplicates().sort_values()
+
+    if(anc_conc_type == 'low'):
+        conc = anchor_conc[0]
+    else:
+        conc = anchor_conc[1]
+
+    anc_df_per_conc = anc_df.loc[anc_df['anchor_conc'] == conc]
+
+    delta_emax_df = get_delta_emax_df(anc_df_per_conc)
+    fig = px.strip(delta_emax_df,x='type',y='Delta Viability (%)',title=' ', labels= {'type':' '})
+    return fig
+
+def generate_delta_ic50_boxplot(anc_df, anc_conc_type):
+    anchor_conc = anc_df['anchor_conc'].drop_duplicates().sort_values()
+
+    if (anc_conc_type == 'low'):
+        conc = anchor_conc[0]
+    else:
+        conc = anchor_conc[1]
+
+    anc_df_per_conc = anc_df.loc[anc_df['anchor_conc'] == conc]
+
+    delta_emax_df = get_delta_ic50_df(anc_df_per_conc)
+    fig = px.strip(delta_emax_df, x='type', y='Delta Norm. drug conc.', title=' ', labels= {'type':' '})
     return fig
 
 def get_emax_df(df,type,col_name):
@@ -136,16 +174,32 @@ def get_emax_df(df,type,col_name):
 
 def get_ic50_df(df,type,col_name):
     type_ic50_df = df[col_name].to_frame()
-
-    # check which method is correct
-    #normalise method 1
-    type_ic50_df = (type_ic50_df - type_ic50_df.mean())/ type_ic50_df.std()
-
-    # normalise method 2
-    # type_ic50_df[col_name].update((type_ic50_df[col_name] - type_ic50_df[col_name].min())/ ( type_ic50_df[col_name].max() - type_ic50_df[col_name].min()))
-    # type_ic50_df[col_name].update((type_ic50_df[col_name] - type_ic50_df[col_name].mean()) / (
-    #             type_ic50_df.std))
     type_ic50_df['type'] = type
-    type_ic50_df = type_ic50_df.rename(columns = { col_name: 'Norm. drug conc'})
+    type_ic50_df = type_ic50_df.rename(columns = { col_name: 'Norm. drug conc.'})
     return type_ic50_df
 
+def get_delta_ic50_df(df):
+    delta_ic50_df = df['synergy_delta_xmid'].to_frame()
+    delta_ic50_df['type'] = 'Delta_IC50'
+    delta_ic50_df = delta_ic50_df.rename(columns = { 'synergy_delta_xmid':'Delta Norm. drug conc.' })
+    return delta_ic50_df
+
+def get_delta_emax_df(df):
+    delta_emax_df = df['synergy_delta_emax'].to_frame()
+    delta_emax_df['type'] = 'Delta_Emax'
+    delta_emax_df['synergy_delta_emax'].update(delta_emax_df['synergy_delta_emax'] * 100)
+    delta_emax_df = delta_emax_df.rename(columns = { 'synergy_delta_emax' : 'Delta Viability (%)'})
+    return delta_emax_df
+
+def get_anc_df(combination):
+    project_id = combination.project_id
+    lib_id = combination.lib1_id
+    anchor_id = combination.lib2_id
+
+    anchor_combi_query = session.query(AnchorCombi). \
+        filter(AnchorCombi.project_id == project_id). \
+        filter(AnchorCombi.library_id == lib_id). \
+        filter(AnchorCombi.anchor_id == anchor_id)
+
+    anc_df = pd.read_sql(anchor_combi_query.statement, session.bind)
+    return anc_df
