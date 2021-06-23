7533389f1e59643d2d49c03c3e3ca639f4f0df17
wy1 <wy1@sanger.ac.uk>
2021-06-23 15:18:31 +0100

    minor changes

--- pages/home.py
+++ pages/home.py
@@ -6,13 +6,13 @@ from models import Project
 
 def layout(*args, **kwargs):
     matrix_projects = session.query(Project).filter(Project.combination_type == 'matrix').all()
-    anchor_projects = session.query(Project).filter(Project.combination_type == 'anchor').all()
+    anchor_projects = session.query(Project).filter(Project.combination_type == 'anchor').order_by(Project.name).all()
 
     return html.Div([
         dbc.Jumbotron(html.Img(src="assets/logo_2tone.svg"), className="d-flex justify-content-center"),
         html.H1("Genomics of Drug Sensitivity in Cancer", className='display-4 text-center'),
         html.H2("Drug Combinations", className="gdsc-heading text-center"),
-        html.P("An interactive tool for the interpretation of bitherapy data generated at the Wellcome Sanger Institute.", className='text-center post-lead'),
+        html.P("An interactive tool for the interpretation of drug combination data generated at the Wellcome Sanger Institute.", className='text-center post-lead'),
         project_card(anchor_projects, 'Anchor projects'),
         project_card(matrix_projects,"Matrix projects")
 
