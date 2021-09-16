b521d1e9ec4158e0bd1e6a1980045ad1f8005365
wy1 <wy1@sanger.ac.uk>
2021-09-16 16:04:11 +0100

    tidy up the code

--- components/anchor_intro.py
+++ components/anchor_intro.py
@@ -10,26 +10,6 @@ from utils import get_combination_from_url, get_project_from_url
 
 
 def layout(project,combination):
-    # combination = get_combination_from_url(url)
-    # project = get_project_from_url(url)
-
-    # query = session.query(AnchorCombi).filter(AnchorCombi.project_id == project.id
-    #         ).filter(AnchorCombi.library_id == combination.lib1.id
-    #                  ).filter(AnchorCombi.anchor_id == combination.lib2.id)
-    #
-    # df = pd.read_sql(query.statement, session.bind)
-    # anchor_conc = df['anchor_conc'].drop_duplicates().sort_values()
-    #
-    # # find the low & high conc
-    # anchor_low = anchor_conc[0]
-    # anchor_high = anchor_conc[1]
-    #
-    # if(anchor_conc[0] > anchor_conc[1]):
-    #     anchor_low = anchor_conc[1]
-    #     anchor_high = anchor_conc[0]
-    #
-    # maxc = df['maxc'].drop_duplicates().sort_values()
-
     return html.Div(
         children=[
             dbc.Row(className="mt-3 mb-3", children=
@@ -54,85 +34,17 @@ def layout(project,combination):
                                          load_intro(project,combination)
                                      )
                                  )
-
-                     #            dbc.Row([
-                     #                dbc.Col(width=6, children=[
-                     #                    dbc.Table(borderless=True, size='sm', children=[
-                     #                        html.Tbody([
-                     #                            html.Tr([
-                     #                                html.Td(html.Strong("Anchor"))
-                     #
-                     #                            ]),
-                     #                            html.Tr([
-                     #                                html.Td(html.Strong("Name")),
-                     #                                html.Td(combination.lib2.name)
-                     #                            ]),
-                     #                            html.Tr([
-                     #                                html.Td(html.Strong("Target")),
-                     #                                html.Td(combination.lib2.target)
-                     #                            ]),
-                     #                            html.Tr([
-                     #                                html.Td(html.Strong("Pathway")),
-                     #                                html.Td(combination.lib2.pathway)
-                     #                            ]),
-                     #
-                     #                            html.Tr([
-                     #                                html.Td(html.Strong("Low conc.")),
-                     #                                html.Td(f"{anchor_low} µM")
-                     #                            ]),
-                     #                            html.Tr([
-                     #                                html.Td(html.Strong("High conc.")),
-                     #                                html.Td(f"{anchor_high} µM")
-                     #                            ])
-                     #                        ])
-                     #                    ])
-                     #                ]),
-                     #
-                     #                dbc.Col(width=6, children=[
-                     #                    dbc.Table(borderless=True, size='sm', children=[
-                     #                        html.Tbody([
-                     #                            html.Tr([
-                     #                                html.Td(html.Strong("Library"))
-                     #
-                     #                            ]),
-                     #                            html.Tr([
-                     #                                html.Td(html.Strong("Name")),
-                     #                                html.Td(combination.lib1.name)
-                     #                            ]),
-                     #                            html.Tr([
-                     #                                html.Td(html.Strong("Target")),
-                     #                                html.Td(combination.lib1.target)
-                     #                            ]),
-                     #                            html.Tr([
-                     #                                html.Td(html.Strong("Pathway")),
-                     #                                html.Td(combination.lib1.pathway)
-                     #                            ]),
-                     #                            html.Tr([
-                     #                                html.Td(html.Strong("Max. conc.")),
-                     #                                html.Td(f"{maxc[0]} µM")
-                     #                            ])
-                     #                        ])
-                     #                    ])
-                     #                ])
-                     #            ])
                       ])
                 ]),
 
             ]),
-          html.Div(className="d-none", id='project-id', children=project.id),
         ]
     )
 
-# @app.callback(
-#     dash.dependencies.Output('intro','children'),
-#     dash.dependencies.Input('project-id', 'children'),
-#     dash.dependencies.State("url", "pathname")
-# )
-
 def load_intro(project,combination):
     query = session.query(AnchorCombi).filter(AnchorCombi.project_id == project.id
-                    ).filter(AnchorCombi.library_id == combination.lib1.id
-                ).filter(AnchorCombi.anchor_id == combination.lib2.id)
+                    ).filter(AnchorCombi.library_id == combination.lib1_id
+                ).filter(AnchorCombi.anchor_id == combination.lib2_id)
 
     df = pd.read_sql(query.statement, session.bind)
     anchor_conc = df['anchor_conc'].drop_duplicates().sort_values()
